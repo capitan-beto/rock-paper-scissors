a681870cda1c4196c331692df2b7350650ee5da7
Otroletravaladna <crnana98@gmail.com>
2022-09-19 18:14:01 -0300

    add play round function.

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function getComputerChoice(){
             return "paper";
     }
 }
+
+function playRound(userChoice, computerSelection){
+    if(userChoice === computerSelection){
+        return "Tie";
+    } else if(userChoice === "rock" && computerSelection === "paper" 
+        || userChoice === "paper" && computerSelection === "scissors" 
+        || userChoice === "scissors" && computerSelection === "paper"){
+            computerScore++;
+            return "Computer has won";
+    } else if(userChoice === "rock" && computerSelection === "scissors"
+        || userChoice === "paper" && computerSelection === "rock"
+        || userChoice === "scissors" && computerSelection === "paper"){
+            playerScore++;
+            return "Player has won";
+    } else{
+        return "Check your spell";
+    }
+}
