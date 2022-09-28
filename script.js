0c6a9312f8cbd8a6280e4b74791da0da43f4a65e
Otroletravaladna <crnana98@gmail.com>
2022-09-28 02:40:40 -0300

    add match winner message

--- script.js
+++ script.js
@@ -28,39 +28,24 @@ function playRound(userInput, computerSelection){
     }
 }
 
-// Variables
+function winner(playerScore, computerScore){
+    if (playerScore === 5){
+        result.textContent = ("Good job boi! You win.");
+    } else if (computerScore === 5){
+        result.textContent = "Dumb-o! You loose.";
+    } else {
+        result.textContent = "";
+    }}
+
+
 let userInput = "";
 let computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
 let roundWinner = "";
-let winner = (playerScore, computerScore) =>{
-    if(computerScore > playerScore){
-        return ("HA! you lost dummy!");
-    } else if (computerScore < playerScore){
-        return ("Good job pal! you won");
-    } else{
-        return ("That was a good match! But it's a tie.")
-    }
-}
+let matchWinner = "";
 
 
-/*for(let i = 0; i < 5; i++){
-    //const userInput = prompt("Type") || "";
-    const userChoice = userInput.toLowerCase();
-    //const computerSelection = getComputerChoice();
-    playRound(userChoice, computerSelection);
-    //console.log(roundWinner);
-    //console.log("your score = " + playerScore);
-    //console.log("Computer's score = " + computerScore);
-    if (i === 4){
-        console.log(winner(playerScore, computerScore)); //try w/ return.
-    }
-}*/
-
-
-
-//Choice selection DOM elements.
 
 const btns = document.querySelector("#btns")
 btns.setAttribute("style", "background: salmon; border: solid 2px red");
@@ -108,11 +93,8 @@ scoreDiv.appendChild(paraComputer);
 
 const result = document.createElement("p");
 result.classList.add("match-winner");
-result.textContent = winner(playerScore, computerScore);
 
 scoreDiv.appendChild(result);
-
-// match
 
 const match = document.querySelectorAll("button");
 match.forEach((button) => {
@@ -121,6 +103,7 @@ match.forEach((button) => {
         paraRound.textContent = `${roundWinner}`;
         paraPlayer.textContent = `Player score: ${playerScore}`;
         paraComputer.textContent = `Computer score: ${computerScore}`;
+        winner(playerScore, computerScore);
     })
 });
 
@@ -128,6 +111,7 @@ const paraRound = document.createElement("p");
 paraRound.classList.add("round-result");
 paraRound.textContent = "Round winner: ";
 btns.appendChild(paraRound);
+
 
 
 
